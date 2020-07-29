d8176a80b49097c5e6fdf8017d8d7bf4b181beda
Jauzy <al.zaujy@gmail.com>
2020-07-29 14:33:38 +0700

    update

--- src/pages/anime_list/index.js
+++ src/pages/anime_list/index.js
@@ -36,24 +36,26 @@ const Combined = (props) => {
 
     useEffect(() => {
         if (mode === 'library') {
-            var makeWinHeight = function () {
-                var vh = $(window).height();
-                var projects = vh / 2;
-                $('#scene, .layer.main').height(vh);
-                $('#scene .col a, #scene .col').height(projects);
+            if (typeof window !== `undefined`) {
+                var makeWinHeight = function () {
+                    var vh = $(window).height();
+                    var projects = vh / 2;
+                    $('#scene, .layer.main').height(vh);
+                    $('#scene .col a, #scene .col').height(projects);
+                }
+                $(document).ready(function () {
+                    makeWinHeight();
+
+                });
+                $(window).resize(function () {
+                    makeWinHeight();
+                });
+
+                var scene = document.getElementById('scene');
+                var parallax = new Parallax(scene, {
+                    hoverOnly: true
+                });
             }
-            $(document).ready(function () {
-                makeWinHeight();
-
-            });
-            $(window).resize(function () {
-                makeWinHeight();
-            });
-
-            var scene = document.getElementById('scene');
-            var parallax = new Parallax(scene, {
-                hoverOnly: true
-            });
         }
     }, [mode])
 

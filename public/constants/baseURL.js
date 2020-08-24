f06b68e4faaea0b7fe90929685edc21119778ae5
Jauzy <al.zaujy@gmail.com>
2020-08-24 09:31:55 +0700

    update

--- public/constants/baseURL.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-// export default axios.create({ baseURL: 'https://jojinime.et.r.appspot.com' })
-// export default axios.create({baseURL: 'http://localhost:80'})
-// export default axios.create({baseURL: 'http://34.101.86.209:5000'})
-export default axios.create({baseURL: 'https://server.jojinime.xyz'})
--- public/redux/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import user from './Reducers/user';
-import anime from './Reducers/anime'
-import schedule from './Reducers/schedule'
-import episode from './Reducers/episode'
-import comment from './Reducers/comment'
-import utils from './Reducers/utils'
-
-export default combineReducers({ user, anime, schedule, episode, comment, utils });
